84288d4e9c9d9d33bd878e94dc538b290e9dd79b
alxn787 <alenabraham787@gmail.com>
2026-02-20 11:28:02 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #[allow(unused_imports)]
 use std::net::TcpListener;
 use std::io::Write;
+use std::io::Read;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -13,11 +14,21 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+                let mut buffer = [0; 1024];
+                let bytes_read = stream.read(&mut buffer).unwrap();
+                println!("Request: {}", String::from_utf8_lossy(&buffer[..bytes_read]));
+                let request = String::from_utf8_lossy(&buffer[..bytes_read]);
+                let path = request.split_whitespace().nth(1).unwrap();
+                let response = match path {
+                    "/" => "HTTP/1.1 200 OK\r\n\r\n",
+                    "/echo" => "HTTP/1.1 200 OK\r\n\r\n",
+                    _ => "HTTP/1.1 404 Not Found\r\n\r\n",
+                };
+                println!("Path: {}", path);
+
+                stream.write_all(response.as_bytes()).unwrap();
             }
-            Err(e) => {
-                println!("error: {}", e);
-            }
+            Err(e) => println!("error: {}", e),
         }
     }
 }
