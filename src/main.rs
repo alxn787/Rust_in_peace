32117225d06e3e6c34d9e8bc8a7ddcb014b509ef
alxn787 <alenabraham787@gmail.com>
2026-02-24 15:18:34 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -20,14 +20,29 @@ fn main() {
                 let request = String::from_utf8_lossy(&buffer[..bytes_read]);
                 let path = request.split_whitespace().nth(1).unwrap();
 
-                let response = if path == "/" {
-                    "HTTP/1.1 200 OK\r\n\r\n".to_string()
-                } else if path.starts_with("/echo/") {
-                    let str = path.split("/").nth(2).unwrap();
-                    let strlen = str.len();
-                    format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", strlen, str).to_string()
-                } else {
-                    "HTTP/1.1 404 Not Found\r\n\r\n".to_string()
+                let response = match path {
+                    "/" => "HTTP/1.1 200 OK\r\n\r\n".to_string(),
+                    p if p.starts_with("/echo/") => {
+                        let echo_str = &p[6..];
+                        format!(
+                            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                            echo_str.len(),
+                            echo_str
+                        )
+                    }
+                    "/user-agent" => {
+                        let user_agent = request
+                            .lines()
+                            .find(|line| line.starts_with("User-Agent: "))
+                            .map(|line| &line[12..])
+                            .unwrap_or("");
+                        format!(
+                            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                            user_agent.len(),
+                            user_agent
+                        )
+                    }
+                    _ => "HTTP/1.1 404 Not Found\r\n\r\n".to_string(),
                 };
 
                 stream.write_all(response.as_bytes()).unwrap();
