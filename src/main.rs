39b5cf470f5d217a904e97b55feb944130009e17
alxn787 <alenabraham787@gmail.com>
2026-03-17 12:17:06 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,30 @@
-#[allow(unused_imports)]
+use std::fs::File;
+use std::io::Read;
+use std::io::Write;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::io::Write;
-use std::io::Read;
 use std::thread;
 
 fn main() {
     println!("Logs from your program will appear here!");
+
+    let args: Vec<String> = std::env::args().collect();
+    
+    let directory_index = args.iter().position(|arg| arg == "--directory"); 
+    
+    // let directory = if let Some(directory_index) = directory_index {
+    //     args[directory_index + 1].clone()
+    // } else {
+    //     ".".to_string()
+    // };
+    // println!("directory: {:?}", directory);
+
+    let directory = match args.iter().position(|a| a== "--directory") {
+        Some(index) => args[index + 1].clone(),
+        None => ".".to_string(),
+    };
+    println!("directory: {:?}", directory);
+
 
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
@@ -15,14 +33,17 @@ fn main() {
 
         match stream {
             Ok(stream) => {
-                thread::spawn(|| {
-                    handle_connection(stream);
+                let dir = directory.clone();
+                thread::spawn(move || {
+                    handle_connection(stream, dir);
                 });
             }
             Err(e) => println!("error: {}", e),
         }
+    }
+}
 
-                pub fn handle_connection(mut stream: TcpStream) {
+fn handle_connection(mut stream: TcpStream, directory: String) {
                         let mut buffer = [0; 1024];
                         let bytes_read = stream.read(&mut buffer).unwrap();
                         println!("Request: {}", String::from_utf8_lossy(&buffer[..bytes_read]));
@@ -51,10 +72,28 @@ fn main() {
                                     user_agent
                                 )
                             }
+                            p if p.starts_with("/files/") => {
+                                let file_path = format!("{}/{}", directory.trim_end_matches('/'), &p[7..]);
+                                let file = File::open(file_path);
+                                match file {
+                                    Ok(mut file) => {
+                                        let mut content = String::new();
+                                        file.read_to_string(&mut content).unwrap();
+                                        format!(
+                                            "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {}\r\n\r\n{}",
+                                            content.len(),
+                                            content
+                                        )
+                                    }
+                                    Err(_e) => {
+                                        format!(
+                                            "HTTP/1.1 404 Not Found\r\n\r\n"
+                                        )
+                                    }
+                                }
+                            }
                             _ => "HTTP/1.1 404 Not Found\r\n\r\n".to_string(),
                         };
 
-                        stream.write_all(response.as_bytes()).unwrap();
-                }
-    }
+    stream.write_all(response.as_bytes()).unwrap();
 }
